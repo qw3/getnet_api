fa88f14b40f54b7ee710c3812c723edcb8028f7e
Leandro Falcao <falcao@qw3.com.br>
2020-03-25 09:16:36 -0300

    New version

--- lib/getnet_api/version.rb
+++ lib/getnet_api/version.rb
@@ -1,9 +1,9 @@
 # -*- encoding : utf-8 -*-
 module GetnetApi
   module Version
-    MAJOR = 0
-    MINOR = 1
-    PATCH = 2
+    MAJOR = 1
+    MINOR = 0
+    PATCH = 0
     STRING = "#{MAJOR}.#{MINOR}.#{PATCH}"
   end
 end
